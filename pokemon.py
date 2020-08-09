37c87314ca08136d66ce29e656389ca44599ea3b
Chris Nuttall <csnuttall@gmail.com>
2020-08-09 18:48:55 -0500

    All functions work. Need to add some logic.

--- pokemon.py
+++ pokemon.py
@@ -1,13 +1,13 @@
 class Pokemon:
     # Create a Pokemon. Give it a name, level, type, maximum health determined by level), and if it's been knocked out.
-    def __init__(self, name, type, level):
+    def __init__(self, name, type, level = 5):
         self.name = name
         self.level = level
-        self.type = type
-        # Max helath is 5 times the pokemon's level.
-        self.max_health = level * 5
         # Pokemon starts at max health.
         self.current_health = level * 5
+        # Max helath is 5 times the pokemon's level.
+        self.max_health = level * 5
+        self.type = type
         # Pokemon starts out conscious and in the fight
         self.knocked_out = False
 
@@ -16,14 +16,15 @@ class Pokemon:
         return "This Level {level} {name} has {health} hit points remaining. {name} is a {type}-type Pokemon.".format(level = self.level, name = self.name, health = self.current_health, type = self.type)
 
     # Deduct health when a pokemon takes damage.
-    def lose_heatlh(self, damage):
+    def lose_health(self, damage):
         self.current_health -= damage
         # Check to see if the damage knocks out the pokemon.
         if self.current_health <= 0:
+            # Can't have negative health.
             self.current_health = 0
             self.knock_out()
         else:
-            print("{name} now has {current_health} out of {max_health} health.".format(name = self.name, current_health = self.current_health, max_health = self.max_health))
+            print("{name} now has {current_health} out of {max_health} health.\n".format(name = self.name, current_health = self.current_health, max_health = self.max_health))
     
     # Add health when a pokemon is healed.
     def gain_health(self, healing):
@@ -36,7 +37,7 @@ class Pokemon:
     # Statement for an unconscious pokemon.
     def knock_out(self):
         self.knocked_out = True
-        print("Oh no! {name} has been knocked out!".format(name = self.name))
+        print("Oh no! {name} has been knocked out!\n".format(name = self.name))
 
     # State for reviving a pokemon.
     def revive(self):
@@ -44,23 +45,24 @@ class Pokemon:
         print("Yay! {name} has been revived and is back in the fight!".format(name = self.name))
 
     def attack(self, other_pokemon):
-        damage = self.level
+        # Base damage is equal to the pokemon's level.
         # If the attacking pokemon has advantage based on type, it deals damage equals to double it's level.
         if (self.type == "Fire" and other_pokemon.type == "Grass") or (self.type == "Water" and other_pokemon.type == "Fire") or (self.type == "Grass" and other_pokemon.type == "Water"):
-            damage = damage * 2
-            other_pokemon.lose_health(damage)
-            print("{attacker_name} attakced {defense_name} for " + damage + " damage.".format(attacker_name = self.name, defense_name = other_pokemon.name))
+            damage = (self.level * 2)
+            print("{attacker_name} attakced {defense_name} for {damage} damage.".format(attacker_name = self.name, defense_name = other_pokemon.name, damage = damage))
             print("It's super effective!")
+            other_pokemon.lose_health(damage)
         # If the attacking pokemon has disadvantage based on type, it deals damage equal to one half it's level.
         if (self.type == "Grass" and other_pokemon.type == "Fire") or (self.type == "Fire" and other_pokemon.type == "Water") or (self.type == "Water" and other_pokemon.type == "Grass"):
-            damage = damage / 2
-            other_pokemon.lose_health(damage)
-            print("{attacker_name} attakced {defense_name} for " + damage + " damage.".format(attacker_name = self.name, defense_name = other_pokemon.name))
+            damage = (self.level / 2)
+            print("{attacker_name} attakced {defense_name} for {damage} damage.".format(attacker_name = self.name, defense_name = other_pokemon.name, damage = damage))
             print("It's not very effective!")
+            other_pokemon.lose_health(damage)
         # If the attacking pokemon has neither advantage or disadvantage on type, it deals damage equal to it's level.
         if (self.type == other_pokemon.type):
+            damage = self.level
+            print("{attacker_name} attakced {defense_name} for {damage} damage.".format(attacker_name = self.name, defense_name = other_pokemon.name, damage = damage))
             other_pokemon.lost_health(damage)
-            print("{attacker_name} attakced {defense_name} for " + damage + " damage.".format(attacker_name = self.name, defense_name = other_pokemon.name))
 
 # Let's make three pokemon. One each of Fire, Grass, and Water.
 # These are subclasses of Pokemon.
@@ -91,14 +93,14 @@ class Trainer:
         print("{name} has the following pokemon: ".format(name = self.name))
         for pokemon in self.pokemons:
             print(pokemon)
-        return "{pokemon} is currently active.".format(pokemon = self.pokemons[self.current_pokemon].name)
+        return "{pokemon} is currently active.\n".format(pokemon = self.pokemons[self.current_pokemon].name)
     
     # A Trainer gives a potion to their pokemon.
     def use_potion(self):
         if self.potions > 0:
             self.pokemons[self.current_pokemon].gain_health(20)
             self.potions -= 1
-            print("{name} just drank a potion. You know have {potions} left.".format(name = self.pokemons[self.current_pokemon].name, potions = self.potions))
+            print("{name} just drank a potion. You now have {potions} left.\n".format(name = self.pokemons[self.current_pokemon].name, potions = self.potions))
         else:
             print("Oh no! You are out of potions!")
 
@@ -111,15 +113,22 @@ class Trainer:
     # Switch pokemon
     def switch_pokemon(self, new_active):
         self.current_pokemon = new_active
-        print("Go {name}, it's your turn!".format(name = self.pokemons[self.current_pokemon].name))
+        print("Go {name}, it's your turn!\n".format(name = self.pokemons[self.current_pokemon].name))
 
 # Make a set of variables for the six pokemon.
+#poke_1 = Charmander()
+#poke_2 = Charmander()
+#poke_3 = Squirtle()
+#poke_4 = Squirtle()
+#poke_5 = Bulbasaur()
+#poke_6 = Bulbasaur()
+
 poke_1 = Charmander()
-poke_2 = Charmander()
+poke_2 = Squirtle()
 poke_3 = Squirtle()
-poke_4 = Squirtle()
-poke_5 = Bulbasaur()
-poke_6 = Bulbasaur()
+poke_4 = Bulbasaur()
+poke_5 = Charmander()
+poke_6 = Squirtle()
 
 # Make two trainers. Each has a list of pokemon, some potions, and a name.
 trainer_1 = Trainer("Blaine", [poke_1, poke_2, poke_3], 4)
@@ -127,3 +136,11 @@ trainer_2 = Trainer("Misty", [poke_4, poke_5, poke_6], 6)
 
 print(trainer_1)
 print(trainer_2)
+
+# Testing attacking, giving potions, and switching pokemon.
+trainer_1.launch_attack(trainer_2)
+trainer_2.launch_attack(trainer_1)
+trainer_2.use_potion()
+trainer_1.launch_attack(trainer_2)
+trainer_2.switch_pokemon(0)
+trainer_2.switch_pokemon(1)
